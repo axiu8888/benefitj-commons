e81b25adb85f8597b080818121ea019615a301e4
dingxiuan <dingxiuan@163.com>
2020-09-28 21:34:02 +0800

    清理无用的代码

--- examples/src/main/java/com/benefitj/examples/Application.java
+++ examples/src/main/java/com/benefitj/examples/Application.java
@@ -2,9 +2,11 @@ package com.benefitj.examples;
 
 import com.benefitj.spring.applicationevent.EnableAutoApplicationListener;
 import com.benefitj.spring.ctx.EnableSpringCtxInit;
+import com.benefitj.spring.eventbus.EnableAutoEventBusPoster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoEventBusPoster
 @EnableSpringCtxInit
 @EnableAutoApplicationListener
 @SpringBootApplication
