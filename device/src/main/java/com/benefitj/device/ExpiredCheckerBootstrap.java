552610530a9f4407189827ac99198f54324b5d0d
dingxiuan <dingxiuan@163.com>
2021-09-06 15:58:56 +0800

    优化代码

--- device/src/main/java/com/benefitj/device/ExpiredCheckerBootstrap.java
+++ device/src/main/java/com/benefitj/device/ExpiredCheckerBootstrap.java
@@ -4,10 +4,42 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.EventListener;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 启动器
  */
 public class ExpiredCheckerBootstrap<T extends Device> extends DefaultExpiredChecker<T> {
+
+  /**
+   * 创建超时检测
+   *
+   * @param manager 设备管理器
+   * @param <T>     设备类型
+   * @return 返回超时检测对象
+   */
+  public static <T extends Device> ExpiredCheckerBootstrap<T> newInstance(DeviceManager<T> manager) {
+    return newInstance(manager, 1, 60, TimeUnit.SECONDS);
+  }
+
+  /**
+   * 创建超时检测
+   *
+   * @param manager 设备管理器
+   * @param delay   延迟时间
+   * @param timeout 超时时长
+   * @param unit    时间单位
+   * @param <T>     设备类型
+   * @return 返回超时检测对象
+   */
+  public static <T extends Device> ExpiredCheckerBootstrap<T> newInstance(DeviceManager<T> manager, int delay, int timeout, TimeUnit unit) {
+    ExpiredCheckerBootstrap<T> bootstrap = new ExpiredCheckerBootstrap<>(manager);
+    bootstrap.setInitialDelay(delay);
+    bootstrap.setDelay(delay);
+    bootstrap.setTimeout(timeout);
+    bootstrap.setUnit(unit);
+    return bootstrap;
+  }
 
   public ExpiredCheckerBootstrap(DeviceManager<T> deviceManager) {
     super(deviceManager);
--- netty/src/main/java/com/benefitj/netty/server/device/NettyDeviceFactory.java
+++ netty/src/main/java/com/benefitj/netty/server/device/NettyDeviceFactory.java
@@ -3,6 +3,7 @@ package com.benefitj.netty.server.device;
 import com.benefitj.device.DeviceFactory;
 import io.netty.channel.Channel;
 
+import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +15,25 @@ import java.util.Map;
 public interface NettyDeviceFactory<T extends NettyDevice> extends DeviceFactory<T> {
 
   String ATTRS_CHANNEL = "channel";
+
+  @Override
+  default T create(String id, @Nullable Map<String, Object> attrs) {
+    Channel ch = null;
+    if (attrs != null && !attrs.isEmpty()) {
+      ch = (Channel) attrs.get(ATTRS_CHANNEL);
+    }
+    return create(id, ch);
+  }
+
+  /**
+   * 创建设备
+   *
+   * @param id      设备ID
+   * @param channel 通道
+   * @return 返回创建的设备
+   */
+  T create(String id, Channel channel);
+
 
   /**
    * wrapper
