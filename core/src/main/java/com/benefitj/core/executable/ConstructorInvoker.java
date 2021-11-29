d4bf6aae7f4f4d5304b277920dd1cb8bd5678040
dingxiuan <dingxiuan@163.com>
2021-11-29 13:58:07 +0800

    添加构造器调用，创建匹配参数的对象

---
+++ core/src/main/java/com/benefitj/core/executable/ConstructorInvoker.java
@@ -0,0 +1,88 @@
+package com.benefitj.core.executable;
+
+import javax.annotation.Nullable;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+public class ConstructorInvoker<T> {
+
+  private ExecutableArgsFinder finder = ExecutableArgsFinder.INSTANCE;
+
+  private final Class<T> klass;
+
+  private Constructor<T>[] constructors;
+
+  public ConstructorInvoker(Class<T> klass) {
+    this.klass = klass;
+    this.constructors = sort((Constructor<T>[]) klass.getConstructors());
+  }
+
+  public ConstructorInvoker(Class<T> klass, ExecutableArgsFinder finder) {
+    this(klass);
+    this.finder = finder;
+  }
+
+  /**
+   * 排序
+   *
+   * @param constructors 构造函数数组
+   * @return 返回排序后的数组
+   */
+  protected Constructor<T>[] sort(Constructor<T>[] constructors) {
+    // 排序，参数多的在前面
+    Arrays.sort(constructors, (o1, o2) ->
+        -Integer.compare(o1.getParameterCount(), o2.getParameterCount()));
+    return constructors;
+  }
+
+  /**
+   * 创建实例对象
+   *
+   * @param provideArgs 可选的参数
+   * @return 返回创建的对象
+   */
+  public T newInstance(@Nullable Object... provideArgs) {
+    try {
+      Constructor<T>[] constructors = getConstructors();
+      if (provideArgs != null && provideArgs.length > 0) {
+        for (Constructor<T> constructor : constructors) {
+          if (constructor.getParameterCount() <= provideArgs.length) {
+            Object[] args = getFinder().find(constructor, provideArgs);
+            if (args != null && args.length > 0) {
+              constructor.setAccessible(true);
+              return constructor.newInstance(args);
+            }
+          }
+        }
+      }
+
+      // 找一个最匹配的构造函数
+      Constructor<T> constructor = constructors[constructors.length - 1];
+      if (constructor.getParameterCount() >= 0) {
+        throw new IllegalStateException("无法找到匹配的对应参数的构造函数!");
+      }
+      constructor.setAccessible(true);
+      return constructor.newInstance();
+    } catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  public Class<T> getKlass() {
+    return klass;
+  }
+
+  public Constructor<T>[] getConstructors() {
+    return constructors;
+  }
+
+  public ExecutableArgsFinder getFinder() {
+    return finder;
+  }
+
+  public void setFinder(ExecutableArgsFinder finder) {
+    this.finder = finder;
+  }
+
+}
--- core/src/test/java/com/benefitj/core/executable/InstantiatorTest.java
+++ core/src/test/java/com/benefitj/core/executable/InstantiatorTest.java
@@ -12,10 +12,23 @@ public class InstantiatorTest extends TestCase {
   @Test
   public void testCreate() {
     Instantiator instantiator = Instantiator.INSTANCE;
-
+    // 创建实例
     TestAbc abc = instantiator.create(TestAbc.class, "abc");
     System.err.println(abc.name);
+  }
 
+  @Test
+  public void testCreate2() {
+    ConstructorInvoker<TestAbc> invoker = new ConstructorInvoker<>(TestAbc.class);
+    // 创建实例
+    TestAbc abc = invoker.newInstance("呵呵");
+    System.err.println(abc.name);
+
+    TestAbc abc2 = invoker.newInstance(11);
+    System.err.println(abc2.name);
+
+    TestAbc abc3 = invoker.newInstance("你好", 13);
+    System.err.println(abc3.name);
   }
 
 
@@ -31,6 +44,14 @@ public class InstantiatorTest extends TestCase {
 
     public TestAbc(String name) {
       this.name = name;
+    }
+
+    public TestAbc(Integer age, String name) {
+      this.name = "age: " + age + ", " + name;
+    }
+
+    public TestAbc(Integer age) {
+      this.name = "age: " + age;
     }
   }
 }
