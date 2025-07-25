f8c63e2134eeca2000635361e4b738ceb079c9b6
dingxiuan <dingxiuan@163.com>
2025-07-25 17:26:32 +0800

    添加获取异常调用栈信息的方法

--- core/src/main/java/com/benefitj/core/CatchUtils.java
+++ core/src/main/java/com/benefitj/core/CatchUtils.java
@@ -4,6 +4,10 @@ import com.benefitj.core.executable.Instantiator;
 import com.benefitj.core.functions.IRunnable;
 import org.apache.commons.lang3.StringUtils;
 
+import javax.annotation.Nullable;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.UnknownHostException;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.function.Consumer;
@@ -148,6 +152,28 @@ public class CatchUtils {
     } catch (Throwable e) {
       return mappedFunc.apply(e);
     }
+  }
+
+  public static String getStackTraceString(@Nullable Throwable tr) {
+    if (tr == null) {
+      return "";
+    }
+
+    // This is to reduce the amount of log spew that apps do in the non-error
+    // condition of the network being unavailable.
+    Throwable t = tr;
+    while (t != null) {
+      if (t instanceof UnknownHostException) {
+        return "";
+      }
+      t = t.getCause();
+    }
+
+    StringWriter sw = new StringWriter();
+    PrintWriter pw = new PrintWriter(sw);
+    tr.printStackTrace(pw);
+    pw.flush();
+    return sw.toString();
   }
 
 }
---
+++ core/src/test/java/com/benefitj/core/CatchUtilsTest.java
@@ -0,0 +1,26 @@
+package com.benefitj.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+@Slf4j
+class CatchUtilsTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void test_getStackTraceString() {
+    String msg = CatchUtils.getStackTraceString(new Exception("呵呵"));
+    log.info(msg);
+  }
+
+}
