368606886176f8fcc783659df3ab9eddaf9e4622
dingxiuan <dingxiuan@163.com>
2025-10-30 12:46:35 +0800

    检测docker运行的系统是否为Windows

--- core/src/main/java/com/benefitj/core/cmd/SystemOS.java
+++ core/src/main/java/com/benefitj/core/cmd/SystemOS.java
@@ -1,5 +1,10 @@
 package com.benefitj.core.cmd;
 
+import com.benefitj.core.*;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
 /**
  * 系统
  */
@@ -10,6 +15,10 @@ public enum SystemOS {
   linux,
   mac,
   ;
+
+  public boolean is_win() {
+    return this == win32 || this == win64;
+  }
 
   public static SystemOS getLocale() {
     if (isWindows()) {
@@ -52,6 +61,46 @@ public enum SystemOS {
   public static String platform(boolean lowercase) {
     String os = System.getProperty("os.name");
     return lowercase ? os.toLowerCase() : os;
+  }
+
+  /**
+   * 监测docker运行的系统
+   */
+  //if grep -qi microsoft /proc/version; then
+  //    echo "宿主机是 Windows (WSL2/Docker Desktop)"
+  //else
+  //    echo "宿主机是 Linux"
+  //fi
+  public static final String DOCKER_PLATFORM_CMD = "\n"
+      + "if grep -qi microsoft /proc/version; then\n"
+      + "    echo \"Windows\"\n"
+      + "else\n"
+      + "    echo \"Linux\"\n"
+      + "fi\n";
+
+  static final SingletonSupplier<Boolean> windowsDocker = SingletonSupplier.of(() -> {
+    CmdCall call = execWindowsDocker();
+    return call != null && call.getMessage().contains("Windows");
+  });
+
+  public static CmdCall execWindowsDocker() {
+    final File tmp = IOUtils.createFile("./DockerPlatform__" + IdUtils.uuid(0, 8) + ".sh");
+    try {
+      IOUtils.write(DOCKER_PLATFORM_CMD.getBytes(StandardCharsets.UTF_8), tmp);
+      return CmdExecutor.get().call("bash " + tmp.getAbsolutePath());
+    } catch (Exception e) {
+      System.err.println(PlaceHolder.get().format("error --> \n{}", CatchUtils.getLogStackTrace(e)));
+    } finally {
+      IOUtils.delete(tmp);
+    }
+    return null;
+  }
+
+  /**
+   * 是否为 windows docker
+   */
+  public static boolean isWindowsDocker() {
+    return isWindows() || windowsDocker.get();
   }
 
 }
